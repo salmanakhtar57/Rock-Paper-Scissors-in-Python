1734049215c7ad8654f10d2a44eb749bd2cd5c51
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-02-03 11:29:09 +0500

    update the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,17 +7,20 @@ def get_computer_choice():
 def play_round(player_selection, computer_selection):
   if player_selection == computer_selection:
     print(f"Both selected {player_selection}. It's a draw.")
+    return 0
   elif player_selection == "rock" and computer_selection == "scissors" or player_selection == "paper" and computer_selection == "rock" or player_selection == "scissors" and computer_selection == "paper":
-    print(f"You choose {player_selection}. Computer Choose {computer_selection} Congrats You win!" )
+    print(f"You choose {player_selection} and computer Choose {computer_selection} \nCongrats You win!" )
+    return 1
   else:
-    print(f"You choose {player_selection}. Computer choose {computer_selection} You lose!")
+    print(f"You choose {player_selection} and computer choose {computer_selection} \nYou lose!")
+    return -1
 
 def game():
   player_score = 0
   computer_score = 0
 
   for i in range(5):
-    player_selection = input("Choose one option: ")
+    player_selection = input("\nPick your choice, 'Rock', 'Paper' or 'Scissors': ")
     computer_selection = get_computer_choice()
 
     result = play_round(player_selection, computer_selection)
@@ -28,10 +31,10 @@ def game():
       computer_score += 1
 
   if player_score > computer_score:
-    print(f"Final score: {player_score} - {computer_score}. You Win!")
+    print(f"\nFinal score: {player_score} - {computer_score}. You won the game!")
   elif player_score < computer_score:
-    print(f"Final score: {player_score} - {computer_score}. You loss!")
+    print(f"\nFinal score: {player_score} - {computer_score}. You loss the game!")
   else:
-    print(f"Final score: {player_score} - {computer_score}. Its a draw")
+    print(f"\nFinal score: {player_score} - {computer_score}. Its a draw")
 
 game()
